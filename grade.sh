27eab976946b49752193e4ca7b816e25a48c19c0
vihsiao <vihsiao@ucsd.edu>
2022-11-16 19:08:25 -0800

    changes

--- grade.sh
+++ grade.sh
@@ -6,7 +6,9 @@ git clone $1 student-submission
 CP=".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar"
 CP2=".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar"
 
-cp student-submission/ListExamples.java ListExamples.java
+cp TestListExamples.java student-submission
+
+cd student-submission
 
 if [[ ! -e ListExamples.java ]]
 then
@@ -14,14 +16,14 @@ then
     exit 1
 fi
 
-#javac -cp $CP *.java 2> err.txt
+javac -cp $CP *.java 2> err.txt
 
-#if [[ $? -eq 0 ]]
-#then
-#    echo "Cannot compile ListExamples and/or TestListExamples!"
-#    cat err.txt
-#    exit 1
-#fi
+if [[ ! $? -eq 0 ]]
+then
+    echo "Cannot compile ListExamples and/or TestListExamples!"
+    cat err.txt
+    exit 1
+fi
 
 #cp TestListExamples.java student-submission
 
@@ -32,10 +34,9 @@ fi
 #echo "file found"
 #fi
 
-javac -cp .:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar *.java
+javac -cp $CP *.java
 java -cp .:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples > output.txt
 
-java -cp .:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples > output.txt
 cat output.txt
 
 exit 0
